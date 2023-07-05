1f5f0e217c0934a4c321fdb5a6e1ac21d4786f13
luke.cha <luke.cha@sendbird.com>
2023-07-05 23:12:04 +0900

    fix for github pages

---
+++ vite.config.pages.ts
@@ -0,0 +1,9 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  // to point to correct path for gh-pages
+  base: '/chat-ai-widget',
+});
