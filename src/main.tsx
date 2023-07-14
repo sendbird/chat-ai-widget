ee90f898a15c441fa59f696de5913c226d66337b
Irene Ryu <irene.ryu@sendbird.com>
2023-07-14 12:56:05 +0900

    feat: move appId & botId env back for easy of use

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const CHAT_WIDGET_APP_ID = import.meta.env.VITE_CHAT_WIDGET_APP_ID;
-const CHAT_WIDGET_BOT_ID = import.meta.env.VITE_CHAT_WIDGET_BOT_ID;
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App
-      applicationId={CHAT_WIDGET_APP_ID}
-      botId={CHAT_WIDGET_BOT_ID}
-    />
+    <App />
   </React.StrictMode>
 );
