de19e9e9c6b421cef2549bce3b9877bb7502a240
Irene Ryu <irene.ryu@sendbird.com>
2023-08-08 16:39:42 +0900

    fix: prevent parent element's scrolling

--- src/hooks/useScrollOnStreaming.ts
+++ src/hooks/useScrollOnStreaming.ts
@@ -13,7 +13,7 @@ export function useScrollOnStreaming({
   bottomBuffer,
 }: Params) {
   const throttledScrollIntoView = useThrottle((element: HTMLDivElement) => {
-    element.scrollIntoView({ block: 'end', behavior: 'smooth' });
+    element.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
     if (bottomBuffer > 0) {
       element.style.scrollMarginBottom = `${bottomBuffer}px`;
     }
