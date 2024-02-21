217ad0bba7725bcff7c6919d7f0cfa11aafc5628
Irene Ryu <irene.ryu@sendbird.com>
2024-02-21 13:59:47 +0900

    Include index.css in index.js

--- packages/self-service/vite.config.ts
+++ packages/self-service/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), cssInjectedByJsPlugin()],
   build: {
     rollupOptions: {
       output: {
+        manualChunks: undefined,
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
         assetFileNames: `[name].[ext]`,
@@ -19,5 +21,5 @@ export default defineConfig({
         'react-code-blocks',
       ],
     },
-  }
+  },
 })
