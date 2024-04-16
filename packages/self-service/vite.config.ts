e981ff57d2c8214d936f5500a15dfe633d37f861
Irene Ryu <irene.ryu@sendbird.com>
2024-04-16 14:24:39 +0900

    chore: modify build output path

--- packages/self-service/vite.config.ts
+++ packages/self-service/vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { getWidgetVersion } from './scripts/widget-version.js';
+
+const version = getWidgetVersion();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), cssInjectedByJsPlugin()],
   build: {
+    outDir: `./dist/${version}`,
     rollupOptions: {
       output: {
         manualChunks: undefined,
