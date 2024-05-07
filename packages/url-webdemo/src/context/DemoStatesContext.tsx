fa265122ccb6e32c390608f27e1a825fc8d0260d
Irene Ryu <irene.ryu@sendbird.com>
2024-05-07 17:22:12 +0900

    chore: remove packages/url-webdemo

--- packages/url-webdemo/src/context/DemoStatesContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-import { DemoConstant, DEMO_CONSTANTS } from '../const';
-
-export const DemoStatesContext = createContext<DemoConstant>(
-  DEMO_CONSTANTS.webDemo
-);
