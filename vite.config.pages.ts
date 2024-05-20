737e02800666abbdc40e8fd101e1b665b9f651b8
bang9 <gusrn1423@naver.com>
2024-05-20 16:49:46 +0900

    ci: enable corepack

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -1,9 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), svgr()],
   // to point to correct path for gh-pages
   base: '/chat-ai-widget',
+  resolve: {
+    alias: [
+      {
+        find: '@uikit',
+        replacement: path.resolve(__dirname, 'packages/uikit/src'),
+      },
+    ],
+  },
 });
