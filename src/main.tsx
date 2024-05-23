52b8dfd82297bc7a4de1f4ebeea1b3f8f482edef
Liam Hongman Cho <liam.cho@sendbird.com>
2024-05-23 12:02:58 +0900

    put back

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,7 @@ import App from './App';
 import { DEFAULT_CONSTANT } from './const';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App firstMessageData={DEFAULT_CONSTANT.firstMessageData} />
+  <React.StrictMode>
+    <App firstMessageData={DEFAULT_CONSTANT.firstMessageData} />
+  </React.StrictMode>
 );
