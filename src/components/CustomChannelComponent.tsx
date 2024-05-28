3a0a4737f805b7462b963ad88d6d33d7a554e965
Liam Hongman Cho <liam.cho@sendbird.com>
2024-05-28 15:48:26 +0900

    apply feeds

--- src/components/CustomChannelComponent.tsx
+++ src/components/CustomChannelComponent.tsx
@@ -187,7 +187,7 @@ export function CustomChannelComponent() {
   });
 
   const blockInputWhileBotResponding =
-    messageInputControlProps?.blockInputWhileBotResponding;
+    messageInputControlProps?.blockWhileBotResponding;
   const isMessageInputDisabled = useDisableInputUntilReply({
     lastMessage,
     botUser,
