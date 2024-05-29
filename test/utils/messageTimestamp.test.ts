b12b33d27e7a2093d0210f379419005d9e36de1b
Liam Hongman Cho <liam.cho@sendbird.com>
2024-05-29 15:56:31 +0900

    update

--- test/utils/messageTimestamp.test.ts
+++ test/utils/messageTimestamp.test.ts
@@ -4,28 +4,10 @@ import { formatCreatedAtToAMPM } from '../../src/utils/messageTimestamp';
 
 describe('formatCreatedAtToAMPM', () => {
   it('should format the time to AM/PM in local timezone (mocked to Germany time)', () => {
-    const mockDate = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+    const mockDate = new Date(Date.UTC(2012, 11, 20, 1, 56, 31));
     // Germany is 2 hours ahead of Coordinated Universal Time
-    const expectedTime = '5:00 PM';
-
-    const originalToLocaleDateString = Date.prototype.toLocaleDateString;
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    Date.prototype.toLocaleDateString = (
-      locale: string,
-      options?: Intl.DateTimeFormatOptions
-    ) => {
-      /**
-       * FIXME: Here is not reached but expected.
-       */
-      // console.log('## locale: ', locale);
-      return originalToLocaleDateString('de-DE', options);
-    };
-
+    const expectedTime = '10:56 AM';
     const result = formatCreatedAtToAMPM(mockDate.getTime());
-    Date.prototype.toLocaleDateString = originalToLocaleDateString;
-
     expect(result).toBe(expectedTime);
   });
 });
