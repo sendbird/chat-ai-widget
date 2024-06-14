29cb1b82d8e12cc4ff843b584ea92bcc3b75f3f2
bang9 <gusrn1423@naver.com>
2024-06-14 13:28:14 +0900

    ci: update gh-pages workflow

--- vite.config.pages.ts
+++ vite.config.pages.ts
@@ -1,10 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import path from "node:path";
+import path from 'node:path';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg',
+      svgrOptions: {
+        exportType: 'default',
+      },
+    }),
+  ],
   // to point to correct path for gh-pages
   base: '/chat-ai-widget',
   resolve: {
