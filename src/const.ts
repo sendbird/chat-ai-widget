401a5e2be90542f26ac9af8d5e8befa5279e07c6
bang9 <gusrn1423@naver.com>
2024-06-20 12:44:32 +0900

    chore: revert failure check

--- src/const.ts
+++ src/const.ts
@@ -351,13 +351,6 @@ export interface CustomRefreshComponent extends CustomRefreshProps {
   icon: React.ComponentType<CustomRefreshProps>;
 }
 
-// ts error
-const x: number = 'str';
-
-// lint error
-var x;
-
-
 export const elementIds = {
   widgetWindow: 'aichatbot-widget-window',
   widgetToggleButton: 'aichatbot-widget-button',
