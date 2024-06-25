03277ac1203fa7c2c71e12b27c1e0c86ea9ed459
Liam Hongman Cho <liam.cho@sendbird.com>
2024-06-25 16:01:36 +0900

    Added image preview

--- src/components/FileMessage.tsx
+++ src/components/FileMessage.tsx
@@ -39,26 +39,6 @@ export default function FileMessage(props: Props) {
    */
   return (
     <div className="sendbird-ai-widget-file-message-root">
-      {/*Please keep the commented code for referencing in the future when adding file viewer*/}
-      {/*
-      {root &&
-        showPreview &&
-        createPortal(
-          <FileViewerComponent
-            profileUrl={message.sender?.profileUrl}
-            nickname={message.sender?.nickname}
-            name={message.name}
-            type={message.type}
-            url={message?.url}
-            isByMe={false}
-            disableDelete={(message.threadInfo?.replyCount || 0) > 0}
-            onClose={() => setShowPreview(false)}
-            onDelete={noop}
-            onDownloadClick={() => downloadFileWithUrl(message.url)}
-          />,
-          root!
-        )}
-        */}
       {isVideoMessage(message) && (
         <video controls className="sendbird-ai-widget-file-message">
           <source src={message.url} type={message.type} />
