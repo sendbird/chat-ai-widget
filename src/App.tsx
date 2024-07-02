a5c2d6a5e9fb604873d862000e8e2b83b09c021e
Hyungu Kang | Airen <gusrn1423@naver.com>
2024-07-02 15:58:39 +0900

    [AC-2970] feat: ignore auto open in mobile view (#296)
    ## Changes
    - ignore autoOpen in mobile view
    
    ticket: [AC-2970]
    
    [AC-2970]:
    https://sendbird.atlassian.net/browse/AC-2970?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,9 @@
-import React from 'react';
+import ChatAiWidget, {
+  ChatAiWidgetProps,
+} from './components/widget/ChatAiWidget';
 
-import { StringSet } from '@uikit/ui/Label/stringSet';
-
-import ChatAiWidget from './components/widget/ChatAiWidget';
-import { ToggleButtonProps } from './components/widget/WidgetToggleButton';
-import { Constant } from './const';
-
-interface Props extends Omit<Partial<Constant>, 'stringSet'> {
-  applicationId: string;
-  botId: string;
-  autoOpen?: boolean;
-  renderWidgetToggleButton?: (props: ToggleButtonProps) => React.ReactElement;
-  stringSet?: Partial<StringSet>;
-}
-
-/**
- * Leave comments below for testing
- */
-const App = (props: Props) => {
-  return (
-    <ChatAiWidget
-      applicationId={props.applicationId}
-      botId={props.botId}
-      userNickName={props.userNickName}
-      betaMark={props.betaMark}
-      customBetaMarkText={props.customBetaMarkText}
-      suggestedMessageContent={props.suggestedMessageContent}
-      firstMessageData={props.firstMessageData}
-      createGroupChannelParams={props.createGroupChannelParams}
-      chatBottomContent={props.chatBottomContent}
-      messageBottomContent={props.messageBottomContent}
-      replacementTextList={props.replacementTextList}
-      customRefreshComponent={props.customRefreshComponent}
-      userId={props.userId}
-      configureSession={props.configureSession}
-      stringSet={props.stringSet}
-      enableSourceMessage={props.enableSourceMessage}
-      enableEmojiFeedback={props.enableEmojiFeedback}
-      enableMention={props.enableMention}
-      customUserAgentParam={props.customUserAgentParam}
-      autoOpen={props.autoOpen}
-      renderWidgetToggleButton={props.renderWidgetToggleButton}
-      serviceName={props.serviceName}
-      deviceType={props.deviceType}
-      enableResetHistoryOnConnect={props.enableResetHistoryOnConnect}
-      // messageInputControls={{
-      //   blockWhileBotResponding: false,
-      // }}
-      // dateLocale={ar}
-      // callbacks={{
-      //   onWidgetExpandStateChange: (newIsExpanded) => {
-      //     console.log('## newIsExpanded: ', newIsExpanded);
-      //   },
-      // }}
-      // botStudioEditProps={{
-      //   botInfo: {
-      //     profileUrl: 'url',
-      //     nickname: 'Super bot',
-      //   },
-      //   aiAttributes: {
-      //     legend: 'this is legend',
-      //   },
-      //   welcomeMessages: [
-      //     {
-      //       message: 'hello',
-      //     },
-      //     {
-      //       message: 'how can I help you?',
-      //     },
-      //     {
-      //       message: 'ewfweweoijweofijfoi',
-      //       suggestedReplies: ['wwfwefwefwefee', 'reply2', 'reply3'],
-      //     },
-      //   ],
-      //   styles: {
-      //     theme: 'light',
-      //     accentColor: '#FF7F50',
-      //     botMessageBGColor: '#ff0095',
-      //   },
-      //   suggestedRepliesDirection: 'vertical',
-      // }}
-      // widgetOpenState={true}
-      // onWidgetOpenStateChange={(props) => {
-      //   console.log('## onChatOpenStateChange: ', props);
-      // }}
-    />
-  );
+const App = (props: ChatAiWidgetProps) => {
+  return <ChatAiWidget {...props} />;
 };
 
 export default App;
