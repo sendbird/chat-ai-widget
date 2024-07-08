e78b786d41e368b8cab4ab07af816980cb074c33
bang9 <gusrn1423@naver.com>
2024-07-09 01:01:03 +0900

    chore: format

--- src/components/widget/WidgetWindow.tsx
+++ src/components/widget/WidgetWindow.tsx
@@ -22,13 +22,19 @@ const StyledWidgetWindowWrapper = styled.div<{
   width: 400px;
   max-width: 80vw;
   max-height: 80vh;
-  box-shadow: 0px 16px 24px 2px rgba(33, 33, 33, 0.12), 0px 6px 30px 5px rgba(33, 33, 33, 0.08),
+  box-shadow:
+    0px 16px 24px 2px rgba(33, 33, 33, 0.12),
+    0px 6px 30px 5px rgba(33, 33, 33, 0.08),
     0px 6px 10px -5px rgba(33, 33, 33, 0.04);
   border-radius: 16px;
   overflow: hidden;
   transform-origin: right bottom;
-  transition: width 200ms ease 0s, height 200ms ease 0s, max-height 200ms ease 0s,
-    transform 150ms cubic-bezier(0, 1.2, 1, 1) 0s, opacity 83ms ease-out 0s;
+  transition:
+    width 200ms ease 0s,
+    height 200ms ease 0s,
+    max-height 200ms ease 0s,
+    transform 150ms cubic-bezier(0, 1.2, 1, 1) 0s,
+    opacity 83ms ease-out 0s;
   transform: scale(0.15);
   opacity: 0;
 
@@ -40,8 +46,12 @@ const StyledWidgetWindowWrapper = styled.div<{
         pointer-events: all;
         transform: scale(1);
         opacity: 1;
-        transition: width 200ms ease 0s, height 200ms ease 0s, max-height 200ms ease 0s,
-          transform 300ms cubic-bezier(0, 1.2, 1, 1) 0s, opacity 83ms ease-out 0s;
+        transition:
+          width 200ms ease 0s,
+          height 200ms ease 0s,
+          max-height 200ms ease 0s,
+          transform 300ms cubic-bezier(0, 1.2, 1, 1) 0s,
+          opacity 83ms ease-out 0s;
       `
     );
   }}
