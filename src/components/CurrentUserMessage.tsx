f2b65220abf6019e6a062ccb0615923f694a08df
Liam Cho <liam.cho@sendbird.com>
2024-07-29 12:08:46 +0900

    Add pending spinner icon for pending messages

--- src/components/CurrentUserMessage.tsx
+++ src/components/CurrentUserMessage.tsx
@@ -1,9 +1,35 @@
 import { UserMessage } from '@sendbird/chat/message';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { DefaultSentTime, BodyContainer, BodyComponent } from './MessageComponent';
 import { useConstantState } from '../context/ConstantContext';
+import Icon from '../icons/spin-icon.svg';
 import { formatCreatedAtToAMPM } from '../utils/messageTimestamp';
+
+const SpinnerSize = '16px';
+
+const spinner = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const IconContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  animation: ${spinner} 1.5s linear infinite;
+  margin-bottom: 2px;
+`;
+
+const SpinnerIcon = styled(Icon)`
+  path {
+    fill: ${({ theme }) => theme.bgColor.outgoingMessage};
+  }
+`;
 
 const Root = styled.div<{ enableEmojiFeedback: boolean }>`
   display: flex;
@@ -24,7 +50,13 @@ export default function CurrentUserMessage(props: Props) {
 
   return (
     <Root enableEmojiFeedback={enableEmojiFeedback}>
-      <DefaultSentTime>{formatCreatedAtToAMPM(message.createdAt, dateLocale)}</DefaultSentTime>
+      {message.sendingStatus === 'pending' ? (
+        <IconContainer>
+          <SpinnerIcon width={SpinnerSize} height={SpinnerSize} />
+        </IconContainer>
+      ) : (
+        <DefaultSentTime>{formatCreatedAtToAMPM(message.createdAt, dateLocale)}</DefaultSentTime>
+      )}
       <BodyContainer>
         <BodyComponent>
           <div className="sendbird-word">{message.message}</div>
