ea75707cec931adc218501919daa18985c437c3a
Hyungu Kang | Airen <gusrn1423@naver.com>
2024-08-07 15:15:05 +0900

    [AC-3505] fix: add root element to full screen component for modal (#329)
    ## Changes
    
    - Fixed a crash when opening a modal in the full-screen widget (root
    element for React portal not found).
    
    ticket: [AC-3505]
    
    [AC-3505]:
    https://sendbird.atlassian.net/browse/AC-3505?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as ChatAiWidget } from './components/widget/ChatAiWidget';
 export { type ProviderContainerProps as ChatAiWidgetConfigs } from './components/widget/ProviderContainer';
-export { default as ChatWindow } from './components/widget/WidgetWindowExternal';
+export { default as ChatWindow } from './components/widget/WidgetWindowFullScreen';
 export { widgetServiceName } from './const';
 export { clearWidgetSessionCache, clearCache } from './libs/storage/widgetSessionCache';
 
