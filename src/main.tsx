58c065de068c9fe015d964c7d3541902a4753b9e
bang9 <gusrn1423@naver.com>
2024-08-27 11:35:12 +0900

    chore: update for qa

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+const QA = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const appId = urlParams.get('app_id') ?? import.meta.env.VITE_CHAT_WIDGET_APP_ID;
+  const botId = urlParams.get('bot_id') ?? import.meta.env.VITE_CHAT_WIDGET_BOT_ID;
+
+  if (!appId || !botId) {
+    return null;
+  }
+
+  return <App applicationId={appId} botId={botId} />;
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App applicationId={import.meta.env.VITE_CHAT_WIDGET_APP_ID} botId={import.meta.env.VITE_CHAT_WIDGET_BOT_ID} />
+    <QA />
   </React.StrictMode>,
 );
