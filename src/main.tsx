29fa43f1f02f3dd7ba55f4febc83c90cc4d7a976
bang9 <gusrn1423@naver.com>
2024-09-09 10:56:27 +0900

    chore: add locale tmp

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,13 @@ const WidgetApp = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const appId = urlParams.get('app_id') ?? import.meta.env.VITE_CHAT_WIDGET_APP_ID;
   const botId = urlParams.get('bot_id') ?? import.meta.env.VITE_CHAT_WIDGET_BOT_ID;
+  const locale = urlParams.get('locale') ?? undefined;
 
   if (!appId || !botId) {
     return null;
   }
 
-  return <App applicationId={appId} botId={botId} />;
+  return <App applicationId={appId} botId={botId} locale={locale} />;
 };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
