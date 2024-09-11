c66c4265b36116c0d03a15a49a606d42ea27bf45
bang9 <gusrn1423@naver.com>
2024-09-11 18:55:46 +0900

    fix: remove props override

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import ChatAiWidget, { ChatAiWidgetProps } from './components/widget/ChatAiWidget';
 
 const App = (props: ChatAiWidgetProps) => {
-  return (
-    <ChatAiWidget
-      {...props}
-      applicationId={'B13D7DE1-7F2B-4343-A896-E812E2BBC67A'}
-      botId={'onboarding_bot'}
-      apiHost={'https://api-no2.sendbirdtest.com'}
-      wsHost={'wss://ws-no2.sendbirdtest.com'}
-    />
-  );
+  return <ChatAiWidget {...props} />;
 };
 
 export default App;
