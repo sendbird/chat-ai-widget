a5e2f82da047b6230de5022c1c448c5509239bf6
bang9 <gusrn1423@naver.com>
2024-10-24 15:57:47 +0900

    feat: update bot profile style for favicon case

--- src/components/BotProfileImage.tsx
+++ src/components/BotProfileImage.tsx
@@ -4,9 +4,35 @@ import { useTheme } from 'styled-components';
 import { useChatContext } from './chat/context/ChatProvider';
 import { getColorBasedOnSaturation } from '../colors';
 import { useConstantState } from '../context/ConstantContext';
+import { themedColors } from '../foundation/colors/css';
 import BotProfileIcon from '../icons/bot-profile-image-small.svg';
 
-const Container = styled.span<{ backgroundColor: string; size: number }>`
+function isMaybeFavicon(url: string) {
+  if (url.length < 4) return false;
+  const fileName = url.substring(url.lastIndexOf('/') + 1);
+  return /fav_|favicon|\.ico/.test(fileName);
+}
+
+const FaviconContainer = styled.div<{ size: number }>`
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  border-radius: 50%;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${themedColors.bg2};
+`;
+
+const FaviconImage = styled.img<{ size: number }>`
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+  box-sizing: border-box;
+  object-fit: contain;
+  padding: 19%;
+`;
+
+const IconContainer = styled.span<{ backgroundColor: string; size: number }>`
   width: ${({ size }) => `${size}px`};
   height: ${({ size }) => `${size}px`};
   background: ${({ backgroundColor }) => backgroundColor};
@@ -24,26 +50,31 @@ const Icon = styled(BotProfileIcon)<{ fill: string }>`
   }
 `;
 
-type Props = {
-  size: number;
-};
-
-function BotProfileImage({ size }: Props) {
+function BotProfileImage({ size }: { size: number }) {
   const theme = useTheme();
 
   const { botStudioEditProps } = useConstantState();
   const { botUser } = useChatContext();
   const { botInfo } = botStudioEditProps ?? {};
+
   const profileUrl = botInfo?.profileUrl ?? botUser?.profileUrl;
 
   if (profileUrl) {
+    if (isMaybeFavicon(profileUrl)) {
+      return (
+        <FaviconContainer size={size}>
+          <FaviconImage size={size} src={profileUrl} alt={'bot profile'} />
+        </FaviconContainer>
+      );
+    }
+
     return <img src={profileUrl} style={{ borderRadius: '50%', width: size, height: size }} alt={'bot profile'} />;
   }
 
   return (
-    <Container size={size} backgroundColor={theme.accentColor}>
+    <IconContainer size={size} backgroundColor={theme.accentColor}>
       <Icon fill={getColorBasedOnSaturation(theme.accentColor)} />
-    </Container>
+    </IconContainer>
   );
 }
 
