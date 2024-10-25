cfd416595487d7fed18fa3dcc5544decce4586de
Hyungu Kang | Airen <gusrn1423@naver.com>
2024-10-25 14:38:19 +0900

    [AC-4071] feat: update bot profile style for favicon case (#379)
    ## Changes
    - update bot profile style for favicon case
    
    ticket: [AC-4071]
    
    ## Additional Notes
    - figma:
    https://www.figma.com/design/KgpsIWkbP3qHdPgRgJaRPW/2409_Product-bumper(Onboarding%2FDashboard)_WIP?node-id=905-24921&node-type=frame&m=dev
    -
    ![image](https://github.com/user-attachments/assets/90f4eaef-1f4c-46e1-a241-76107ffc16b8)
    
    
    ## Checklist
    Before requesting a code review, please check the following:
    - [x] **[Required]** CI has passed all checks.
    - [x] **[Required]** A self-review has been conducted to ensure there
    are no minor mistakes.
    - [x] **[Required]** Unnecessary comments/debugging code have been
    removed.
    - [x] **[Required]** All requirements specified in the ticket have been
    accurately implemented.
    - [x] Ensure the ticket has been updated with the sprint, status, and
    story points.
    
    
    [AC-4071]:
    https://sendbird.atlassian.net/browse/AC-4071?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,24 @@ const WidgetApp = () => {
   const appId = urlParams.get('app_id') ?? import.meta.env.VITE_CHAT_WIDGET_APP_ID;
   const botId = urlParams.get('bot_id') ?? import.meta.env.VITE_CHAT_WIDGET_BOT_ID;
   const locale = urlParams.get('locale') ?? undefined;
+  const region = urlParams.get('region') ?? undefined;
+
+  function getHost(region?: string) {
+    if (region && region.startsWith('no')) {
+      return { apiHost: `https://api-${region}.sendbirdtest.com`, wsHost: `wss://ws-${region}.sendbirdtest.com` };
+    }
+    return {
+      apiHost: region ? `https://api-cf-${region}.sendbird.com` : undefined,
+      wsHost: undefined,
+    };
+  }
 
   if (!appId || !botId) {
     return null;
   }
 
-  return <App applicationId={appId} botId={botId} locale={locale} />;
+  const host = getHost(region);
+  return <App applicationId={appId} botId={botId} locale={locale} apiHost={host.apiHost} wsHost={host.wsHost} />;
 };
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
