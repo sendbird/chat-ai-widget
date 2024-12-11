ad9576f4d8ca365078442f60c5de875f359c02c8
Liam Cho <liam.cho@sendbird.com>
2024-12-11 13:20:08 +0900

    fix

--- __visual_tests__/workflow-tests.spec.ts
+++ __visual_tests__/workflow-tests.spec.ts
@@ -1,13 +1,7 @@
 import { test } from '@playwright/test';
 
 import { WidgetComponentIds } from './const';
-import {
-  assertScreenshot,
-  clickNthChip,
-  deleteTestResources,
-  loadWidget,
-  sendTextMessage,
-} from './utils/testUtils';
+import { assertScreenshot, clickNthChip, deleteTestResources, loadWidget, sendTextMessage } from './utils/testUtils';
 
 test.afterEach(async ({ page }) => {
   await deleteTestResources(page);
@@ -157,7 +151,7 @@ test('104', async ({ page, browserName }) => {
   const fallback = page.locator(WidgetComponentIds.MARKDOWN);
   await fallback.first().waitFor({ state: 'visible' });
   let options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
-  await options.nth(3).waitFor({ state: 'visible' });
+  await options.nth(4).waitFor({ state: 'visible' });
   await assertScreenshot(page, '104-1', browserName);
 
   // 2
@@ -167,7 +161,7 @@ test('104', async ({ page, browserName }) => {
   await assertScreenshot(page, '104-2', browserName);
   await options.nth(0).click(); // Go back
   options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
-  await options.nth(3).waitFor({ state: 'visible' });
+  await options.nth(4).waitFor({ state: 'visible' });
 
   // 3
   await options.nth(1).click();
@@ -176,7 +170,7 @@ test('104', async ({ page, browserName }) => {
   await assertScreenshot(page, '104-3', browserName);
   await options.nth(0).click(); // Go back
   options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
-  await options.nth(3).waitFor({ state: 'visible' });
+  await options.nth(4).waitFor({ state: 'visible' });
 
   // 4
   await options.nth(2).click();
@@ -185,11 +179,20 @@ test('104', async ({ page, browserName }) => {
   await assertScreenshot(page, '104-4', browserName);
   await options.nth(0).click(); // Go back
   options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
-  await options.nth(3).waitFor({ state: 'visible' });
+  await options.nth(4).waitFor({ state: 'visible' });
 
   // 5
   await options.nth(3).click();
   options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
   await options.nth(0).waitFor({ state: 'visible' }); // Wait for go back button to show
   await assertScreenshot(page, '104-5', browserName);
+  await options.nth(0).click(); // Go back
+  options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
+  await options.nth(4).waitFor({ state: 'visible' });
+
+  // 6
+  await options.nth(4).click();
+  options = page.locator(WidgetComponentIds.SUGGESTED_REPLIES_OPTIONS);
+  await options.nth(0).waitFor({ state: 'visible' }); // Wait for go back button to show
+  await assertScreenshot(page, '104-6', browserName);
 });
