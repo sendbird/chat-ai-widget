4f2575585dd45097d88e6174e50026aab570f3c8
Liam Cho <liam.cho@sendbird.com>
2025-01-20 15:26:45 +0900

    fix: Update useStyledComponentsTarget to support case where head tag is removed.

--- src/hooks/useStyledComponentsTarget.ts
+++ src/hooks/useStyledComponentsTarget.ts
@@ -9,7 +9,9 @@ function isSCTarget(node: Node): node is HTMLStyleElement {
  * This hook observes mutations in the document's head
  * When styled-components, which has already been initialized, is re-added to the head, for example `document.head.innerHTML += ''`, the styles may not render correctly.
  * Therefore, the target is moved to the body tag.
- *
+ * Similarly, the issue could also rise in below cases and the hook handles them accordingly:
+ * - If <head> is removed, switch to <body>.
+ * - If styles are removed from <head>, switch to <body>.
  * This is a short-term solution, and in the long run, we plan to remove styled-components altogether.
  * */
 export function useStyledComponentsTarget() {
@@ -18,19 +20,42 @@ export function useStyledComponentsTarget() {
 
   useLayoutEffect(() => {
     const observer = new MutationObserver((mutations) => {
-      const mutation = mutations.find((it) => it.target === document.head && it.addedNodes.length > 0);
-
-      for (const node of mutation?.addedNodes ?? []) {
-        if (!isSCTarget(node)) continue;
-
-        if (scInitialized.current) {
-          setTarget(document.body);
-        } else {
-          scInitialized.current = true;
-        }
+      // Case 1: Detect if <head> is removed
+      if (!document.head) {
+        console.warn('document.head was removed, switching to <body>');
+        setTarget(document.body);
+        return;
       }
+      mutations.forEach((mutation) => {
+        // Case 2: Detect if styles are added to <head>
+        if (mutation.addedNodes.length > 0) {
+          for (const node of mutation.addedNodes) {
+            if (isSCTarget(node)) {
+              if (scInitialized.current) {
+                console.warn('Styled Components styles re-injected, switching to <body>');
+                setTarget(document.body);
+              } else {
+                scInitialized.current = true;
+              }
+              return;
+            }
+          }
+        }
+        // Case 3: Detect if styles are removed from <head>
+        if (mutation.removedNodes.length > 0) {
+          for (const node of mutation.removedNodes) {
+            if (isSCTarget(node)) {
+              console.warn('Styled Components styles removed, switching to <body>');
+              setTarget(document.body);
+              return;
+            }
+          }
+        }
+      });
     });
+
     observer.observe(document.head, { childList: true });
+
     return () => observer.disconnect();
   }, []);
 
