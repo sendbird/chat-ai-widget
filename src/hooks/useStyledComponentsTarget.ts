9d27c0a1bc000b7d5b1bbaa92240d55c07c894e3
Liam Cho <liam.cho@sendbird.com>
2025-01-24 11:20:33 +0900

    minor

--- src/hooks/useStyledComponentsTarget.ts
+++ src/hooks/useStyledComponentsTarget.ts
@@ -12,18 +12,24 @@ function isSCTarget(node: Node): node is HTMLStyleElement {
  * When styled-components, which has already been initialized, is re-added to the head, for example `document.head.innerHTML += ''`, the styles may not render correctly.
  * Therefore, the target is moved to the body tag.
  * Similarly, the issue could also rise in below cases and the hook handles them accordingly:
- * - If styles are removed from <head>, switch to <body>.
+ * - If styles are removed from <head>, re-add to head if head exists or switch to <body>.
  * This is a short-term solution, and in the long run, we plan to remove styled-components altogether.
  * */
 export function useStyledComponentsTarget() {
   const [target, setTarget] = useState(document.head);
 
   useLayoutEffect(() => {
-    const moveStyleToBody = (styleElement: HTMLElement) => {
+    const handleRemovedStyle = (styleElement: HTMLElement) => {
       if (styleElement && styleElement.parentElement !== document.body) {
-        console.warn(`[useStyledComponentsTarget]: Moving style element ${StyledId} to <body>.`);
-        document.body.appendChild(styleElement);
-        setTarget(document.body);
+        if (document.head) {
+          console.warn('[useStyledComponentsTarget]: Head exists, re-adding style element ${StyledId} to <head>.');
+          document.head.appendChild(styleElement);
+          setTarget(document.head);
+        } else {
+          console.warn('[useStyledComponentsTarget]: Head missing, moving style element ${StyledId} to <body>.');
+          document.body.appendChild(styleElement);
+          setTarget(document.body);
+        }
       }
     };
 
@@ -43,7 +49,7 @@ export function useStyledComponentsTarget() {
             console.warn('[useStyledComponentsTarget]: Styled Components styles removed, switching to <body>');
             setTarget(document.body);
           } else if (node instanceof HTMLElement && node.id === StyledId) {
-            moveStyleToBody(node);
+            handleRemovedStyle(node);
           }
         });
       });
